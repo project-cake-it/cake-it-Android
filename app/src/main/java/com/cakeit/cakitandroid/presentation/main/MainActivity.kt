507ce1413b39bcadfe6c3e6ac7a5a62cd4887eed
seong4862 <nuli95@naver.com>
2021-04-11 01:48:31 +0900

    가게 상세 페이지 UI 추가 - feat

--- app/src/main/java/com/cakeit/cakitandroid/presentation/main/MainActivity.kt
+++ app/src/main/java/com/cakeit/cakitandroid/presentation/main/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.cakeit.cakitandroid.presentation.main
 
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import androidx.lifecycle.ViewModelProvider
 import com.cakeit.cakitandroid.R
 import com.cakeit.cakitandroid.base.BaseActivity
 import com.cakeit.cakitandroid.databinding.ActivityMainBinding
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
 
@@ -16,6 +22,7 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
         dataBinding()
         binding = getViewDataBinding()
         binding.viewModel = getViewModel()
+        getHashKey()
     }
     override fun getLayoutId(): Int {
         return R.layout.activity_main
@@ -24,5 +31,24 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
     override fun getViewModel(): MainViewModel {
         mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         return mainViewModel
+    }
+
+    fun getHashKey() {
+        var packageInfo: PackageInfo? = null
+        try {
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES)
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        }
+        if (packageInfo == null) Log.e("KeyHash", "KeyHash:null")
+        for (signature in packageInfo!!.signatures) {
+            try {
+                val md: MessageDigest = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+            } catch (e: NoSuchAlgorithmException) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=$signature", e)
+            }
+        }
     }
 }
