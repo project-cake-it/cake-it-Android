d5ffaf828fd6302089a149e28db07af90ce6ca38
DESKTOP-RPF0EIL\82103 <jemin56@naver.com>
2021-04-13 23:52:00 +0900

    Add Shop Detail UI(Contstraint Layout)

---
+++ app/src/main/java/com/cakeit/cakitandroid/presentation/shop/ShopActivity.kt
@@ -0,0 +1,11 @@
+package com.cakeit.cakitandroid.presentation.shop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShopActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop)
+    }
+}
