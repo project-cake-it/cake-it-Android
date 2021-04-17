6fad5d9a620147641c06db4ca191b5a1364804f6
Sungmin Lee <starmin114@yonsei.ac.kr>
2021-04-17 13:00:47 +0900

    BaseUseCase varargs 추가

--- app/src/main/java/com/cakeit/cakitandroid/domain/usecase/base/BaseUseCase.kt
+++ app/src/main/java/com/cakeit/cakitandroid/domain/usecase/base/BaseUseCase.kt
@@ -6,15 +6,16 @@ import io.reactivex.schedulers.Schedulers
 
 abstract class BaseUseCase<T> : UseCase() {
 
-    internal abstract fun buildUseCase(): Single<T>
+    internal abstract fun buildUseCase(vararg args : Any?): Single<T>
 
     fun execute(
+        vararg args: Any?,
         onSuccess: ((t: T) -> Unit),
         onError: ((t: Throwable) -> Unit),
         onFinished: () -> Unit = {}
     ) {
         disposeLast()
-        lastDisposable = buildUseCase()
+        lastDisposable = buildUseCase(args)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .doAfterTerminate(onFinished)
