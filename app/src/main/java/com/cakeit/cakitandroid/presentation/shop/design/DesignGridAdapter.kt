d36212a07b436bbfd0a904295ab2a431bc6b8f93
seong4862 <nuli95@naver.com>
2021-06-10 00:57:07 +0900

    카카오 지도  추가 - feat

--- app/src/main/java/com/cakeit/cakitandroid/presentation/shop/design/DesignGridAdapter.kt
+++ app/src/main/java/com/cakeit/cakitandroid/presentation/shop/design/DesignGridAdapter.kt
@@ -1,4 +1,61 @@
 package com.cakeit.cakitandroid.presentation.shop.design
 
-class DesignGridAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.cakeit.cakitandroid.R
+
+// TODO : cakeShopDatas  >  emptyList 제네릭 타입 수정 필
+
+class DesignGridAdapter(context: Context) : RecyclerView.Adapter<DesignGridAdapter.ItemViewHolder>() {
+
+    private lateinit var onItemClick : View.OnClickListener
+    private var designPhotoDatas = emptyList<String>()
+    private val context = context
+
+    inner class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    {
+        private val designPhoto: ImageButton = itemView.findViewById(R.id.ib_design_grid_photo)
+
+        fun bind(data: String,  context: Context)
+        {
+            if(!data.isNullOrEmpty())
+            {
+//                if(!data.imgUrl.isNullOrEmpty())
+//                {
+//                    Glide.with(context).load(data.imgUrl).into(designPhoto)
+//                }
+                Glide.with(context).load(R.drawable.test).into(designPhoto)
+            }
+        }
+    }
+
+    internal fun setRefresh(designPhotoDatas: ArrayList<String>) {
+        this.designPhotoDatas = designPhotoDatas
+        notifyDataSetChanged()
+    }
+
+    fun setOnItemClickListener(l: View.OnClickListener) {
+        onItemClick = l
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DesignGridAdapter.ItemViewHolder {
+        val view : View = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_design_grid, parent, false)
+
+        view.setOnClickListener(onItemClick)
+
+        return ItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: DesignGridAdapter.ItemViewHolder, position: Int) {
+        holder.bind(designPhotoDatas[position], context)
+    }
+
+    override fun getItemCount(): Int = designPhotoDatas.size
+
 }
--- app/src/main/java/com/cakeit/cakitandroid/presentation/shop/inform/ShopInformFragment.kt
+++ app/src/main/java/com/cakeit/cakitandroid/presentation/shop/inform/ShopInformFragment.kt
@@ -3,10 +3,13 @@ package com.cakeit.cakitandroid.presentation.shop.inform
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import com.cakeit.cakitandroid.R
 import com.cakeit.cakitandroid.base.BaseFragment
 import com.cakeit.cakitandroid.databinding.FragmentShopInformBinding
+import kotlinx.android.synthetic.main.fragment_shop_inform.*
+import net.daum.mf.map.api.MapView
 
 class ShopInformFragment : BaseFragment<FragmentShopInformBinding, ShopInformFragmentViewModel>() {
 
@@ -16,9 +19,10 @@ class ShopInformFragment : BaseFragment<FragmentShopInformBinding, ShopInformFra
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        Log.d("songjem", "here shopinfo")
         binding = getViewDataBinding()
         binding.viewModel = getViewModel()
+
+        loadKakaoMap()
     }
 
     override fun getLayoutId(): Int {
@@ -28,5 +32,13 @@ class ShopInformFragment : BaseFragment<FragmentShopInformBinding, ShopInformFra
     override fun getViewModel(): ShopInformFragmentViewModel {
         shopInformFragmentViewModel = ViewModelProvider(this).get(ShopInformFragmentViewModel::class.java)
         return shopInformFragmentViewModel
+    }
+
+    fun loadKakaoMap()
+    {
+        val mapView = MapView(activity)
+        val mapViewContainer = iv_shop_inform_map as ViewGroup
+
+        mapViewContainer.addView(mapView)
     }
 }
