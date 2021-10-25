86849b5425167f5c7dbd31fc93a8984e0d09ee34
DESKTOP-RPF0EIL\82103 <jemin56@naver.com>
2021-10-26 02:47:02 +0900

    검색 Fragment 강제 포커스

--- app/src/main/java/com/cakeit/cakitandroid/presentation/search/SearchFragment.kt
+++ app/src/main/java/com/cakeit/cakitandroid/presentation/search/SearchFragment.kt
@@ -1,11 +1,14 @@
 package com.cakeit.cakitandroid.presentation.search
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.View.OnFocusChangeListener
 import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.TextView.OnEditorActionListener
 import androidx.lifecycle.ViewModelProvider
 import com.cakeit.cakitandroid.R
@@ -14,11 +17,13 @@ import com.cakeit.cakitandroid.databinding.FragmentSearchBinding
 import com.cakeit.cakitandroid.presentation.search.searchlist.SearchListActivity
 import kotlinx.android.synthetic.main.fragment_search.*
 
+
 class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>() {
 
     lateinit var keyword : String
     lateinit var binding : FragmentSearchBinding
     lateinit var searchViewModel : SearchViewModel
+    var myIsVisibleToUser : Boolean = false
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -49,9 +54,28 @@ class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>() {
             false
         })
 
+        et_search_input.setOnFocusChangeListener(OnFocusChangeListener { view, hasFocus ->
+            if (hasFocus) {
+            } else hideKeyboard()
+        })
+
         btn_search_clear.setOnClickListener {
             et_search_input.setText("")
+            showKeyboard()
             btn_search_clear.visibility = View.INVISIBLE
+        }
+
+        rl_search_content.setOnClickListener {
+            hideKeyboard()
+        }
+    }
+
+    override fun setUserVisibleHint(isVisibleToUser: Boolean) {
+        super.setUserVisibleHint(isVisibleToUser)
+        myIsVisibleToUser = isVisibleToUser
+
+        if (isVisibleToUser && getActivity()!= null) {
+            showKeyboard()
         }
     }
 
@@ -62,5 +86,19 @@ class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>() {
     override fun getViewModel(): SearchViewModel {
         searchViewModel = ViewModelProvider(this).get(SearchViewModel::class.java)
         return searchViewModel
+    }
+
+    fun showKeyboard() {
+        et_search_input.isFocusableInTouchMode()
+        et_search_input.setFocusable(true)
+        et_search_input.requestFocus()
+
+        val imm = activity!!.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+        imm!!.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY)
+    }
+
+    fun hideKeyboard() {
+        var imm = activity!!.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view!!.windowToken, 0);
     }
 }
